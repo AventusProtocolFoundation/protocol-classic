1cbf236f81343e7fe144429ae6b670295e816c3d
Andy <andy.mcewan@artos.io>
2019-10-07 12:39:26 +0100

    Various changes to get script running on rinkeby

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ This is the Classic form of the Aventus Protocol.
 
 * See [here](https://github.com/AventusProtocolFoundation/protocol/blob/master/README.md) for the non-classic version of the protocol.
 
+# Rinkeby changes since deployment
+
+2019-10-07: AVT deposits for events on Rinkeby have been reduced to be inline with the faucet drip:
+- free event is now [10AVT](https://rinkeby.etherscan.io/tx/0xebe0a7c62b7c3f656f795ff812e73065318a31820bae62f138db38c5565e7658)
+- non-free event [20AVT](https://rinkeby.etherscan.io/tx/0xf59b19e5e8cae6f30a8d1c29d796a0c7d460c23bebd8c04ac1bb90c414fb4fb3)
+
 # Rinkeby contract addresses
 
 - **AventusStorage** [0x929d67079aA9786C45e1454B25d4Fa7957b1B208](https://rinkeby.etherscan.io/address/0x929d67079aA9786C45e1454B25d4Fa7957b1B208)
--- scripts/ChangeStorageValue.js
+++ scripts/ChangeStorageValue.js
@@ -1,4 +1,3 @@
-const EventsManager = require('../AventusClassicJS/EventsManager.js');
 const Web3ContractsManager = require('../AventusClassicJS/Web3ContractsManager.js');
 const AventusAPI = require('../AventusClassicJS/AventusAPI.js');
 
@@ -6,27 +5,24 @@ async function main() {
   console.log('Starting AVT Reference App');
   const contractsManager = new Web3ContractsManager();
   await contractsManager.initialise('rinkeby');
-
   const aventusApi = new AventusAPI();
   await aventusApi.initialise(contractsManager);
-
   const BN = web3.utils.BN;
   console.log('*** ...done***\n');
 
   let params = aventusApi.parameters;
 
   const key = aventusApi.hash('Events' + 'PaidEventDepositAmount');
-  const value = (new BN(20)).mul(new BN(10).pow(new BN(18)));
+  const value = (new BN(20)).mul(new BN(10).pow(new BN(18))).toString();
 
-  params.gas = await aventusApi.aventusStorage.methods.setUInt(key, value.toString()).estimateGas(params);
+  params.gas = await aventusApi.aventusStorage.methods.setUInt(key, value).estimateGas(params);
   params.gasPrice = await web3.eth.getGasPrice();
 
   // Change this to be something reasonable if using a public net: eg see ethgastation.info
   console.log('Using gas price', params.gasPrice);
   const weiRequired = params.gasPrice * 1; // params.gas;
   const existingBalance = await web3.eth.getBalance(params.from);
-  //
-  // console.log('Wei required  =', weiRequired);
+  console.log('Wei required  =', weiRequired);
   console.log('Balance =', existingBalance);
 
   if (weiRequired > existingBalance) {
@@ -35,11 +31,13 @@ async function main() {
     console.log('Balance should be enough');
   }
 
-  console.log('Setting key:', key, 'to value:', value.toString(), '...');
+  console.log('Setting key:', key, 'to value:', value, '...');
   try {
-    console.log('Before:', await aventusApi.aventusStorage.methods.getUInt(key).call());
-    await aventusApi.aventusStorage.methods.setUInt(key, value.toString()).send(params);
-    console.log('After :', await aventusApi.aventusStorage.methods.getUInt(key).call());
+    console.log('Before     :', await aventusApi.aventusStorage.methods.getUInt(key).call());
+    console.log('Changing to:', value);
+    // WARNING: Uncomment this line to make the actual change.
+    // await aventusApi.aventusStorage.methods.setUInt(key, value).send(params);
+    console.log('After      :', await aventusApi.aventusStorage.methods.getUInt(key).call());
     console.log('...done');
   } catch (e) {
     console.log('ERROR:', e);
