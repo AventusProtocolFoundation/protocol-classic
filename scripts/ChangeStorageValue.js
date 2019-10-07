385ac456be8966afaeb81d0e249a5e16a3d0e080
Andy <andy.mcewan@artos.io>
2019-10-07 10:31:34 +0100

    Add script to change values in storage: enable lowering of deposits

---
+++ scripts/ChangeStorageValue.js
@@ -0,0 +1,51 @@
+const EventsManager = require('../AventusClassicJS/EventsManager.js');
+const Web3ContractsManager = require('../AventusClassicJS/Web3ContractsManager.js');
+const AventusAPI = require('../AventusClassicJS/AventusAPI.js');
+
+async function main() {
+  console.log('Starting AVT Reference App');
+  const contractsManager = new Web3ContractsManager();
+  await contractsManager.initialise('rinkeby');
+
+  const aventusApi = new AventusAPI();
+  await aventusApi.initialise(contractsManager);
+
+  const BN = web3.utils.BN;
+  console.log('*** ...done***\n');
+
+  let params = aventusApi.parameters;
+
+  const key = aventusApi.hash('Events' + 'PaidEventDepositAmount');
+  const value = (new BN(20)).mul(new BN(10).pow(new BN(18)));
+
+  params.gas = await aventusApi.aventusStorage.methods.setUInt(key, value.toString()).estimateGas(params);
+  params.gasPrice = await web3.eth.getGasPrice();
+
+  // Change this to be something reasonable if using a public net: eg see ethgastation.info
+  console.log('Using gas price', params.gasPrice);
+  const weiRequired = params.gasPrice * 1; // params.gas;
+  const existingBalance = await web3.eth.getBalance(params.from);
+  //
+  // console.log('Wei required  =', weiRequired);
+  console.log('Balance =', existingBalance);
+
+  if (weiRequired > existingBalance) {
+    console.log('*** WARNING: weiRequired > existingBalance: transaction may fail');
+  } else {
+    console.log('Balance should be enough');
+  }
+
+  console.log('Setting key:', key, 'to value:', value.toString(), '...');
+  try {
+    console.log('Before:', await aventusApi.aventusStorage.methods.getUInt(key).call());
+    await aventusApi.aventusStorage.methods.setUInt(key, value.toString()).send(params);
+    console.log('After :', await aventusApi.aventusStorage.methods.getUInt(key).call());
+    console.log('...done');
+  } catch (e) {
+    console.log('ERROR:', e);
+  }
+
+  aventusApi.tearDown();
+}
+
+return main();
